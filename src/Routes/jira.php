bac32c78f1231e1e453ee2b78987eb9d447c111f
Arrow <36331175+masterarrow@users.noreply.github.com>
2023-01-16 21:23:36 +0200

    Delete jira.php

--- src/Routes/jira.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('atlassian/jira/oauth/access', 'OAuthController@requestToken')
-    ->name('atlassian.jira.oauth.access');
-
-Route::get('atlassian/jira/oauth/callback', 'OAuthController@callback')
-    ->name('atlassian.jira.oauth.callback');
