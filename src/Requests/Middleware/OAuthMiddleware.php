a48470e3b1f348aaac0df8715fc7081d896386d5
Arrow <36331175+masterarrow@users.noreply.github.com>
2023-01-16 20:27:26 +0200

    OAuth 2.0

--- src/Requests/Middleware/OAuthMiddleware.php
+++ src/Requests/Middleware/OAuthMiddleware.php
@@ -7,6 +7,9 @@ use Closure;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
+use kamermans\OAuth2\GrantType\ClientCredentials;
+use kamermans\OAuth2\OAuth2Subscriber;
+
 class OAuthMiddleware
 {
     /**
@@ -19,7 +22,7 @@ class OAuthMiddleware
      */
     public function handle($options, Closure $next)
     {
-        $stack = HandlerStack::create();
+        /*$stack = HandlerStack::create();
 
         if (config(Provider::CONFIG_KEY.'.auth.oauth.impersonate')) {
             $userId = null;
@@ -65,6 +68,53 @@ class OAuthMiddleware
         $options['auth'] = 'oauth';
         $options['handler'] = $stack;
 
+        return $next($options);*/
+
+        $stack = HandlerStack::create();
+
+        $scopes = ['offline_access', 'read:me', 'read:account', 'report:personal-data', 'read:jira-user',
+            'read:jira-work', 'write:jira-work', 'manage:jira-project', 'manage:jira-configuration',
+            'manage:jira-webhook', 'manage:jira-data-provider'];
+
+        $reauth_config = [
+            "client_id" => env('JIRA_CLIENT_ID'),
+            "client_secret" => env('JIRA_SECRET'),
+            "scope" => $scopes,
+            "state" => time()
+        ];
+
+        $middleware = new OAuth2Subscriber($reauth_config);
+
+        $stack->push($middleware);
+
+        $options['auth'] = 'oauth2';
+        $options['handler'] = $stack;
+
         return $next($options);
+
+        /*
+        // Authorization client - this is used to request OAuth access tokens
+        $reauth_client = new GuzzleHttp\Client([
+            // URL for access_token request
+            'base_url' => 'http://some_host/access_token_request_url',
+        ]);
+        $reauth_config = [
+            "client_id" => "your client id",
+            "client_secret" => "your client secret",
+            "scope" => "your scope(s)", // optional
+            "state" => time(), // optional
+        ];
+        $grant_type = new ClientCredentials($reauth_client, $reauth_config);
+        $oauth = new OAuth2Subscriber($grant_type);
+
+        // This is the normal Guzzle client that you use in your application
+        $client = new GuzzleHttp\Client([
+            'auth' => 'oauth',
+        ]);
+        $client->getEmitter()->attach($oauth);
+        $response = $client->get('http://somehost/some_secure_url');
+
+        echo "Status: ".$response->getStatusCode()."\n";
+        */
     }
 }
