65989d0f616a126017ba4587850b8aa7ccf54523
Arrow <36331175+masterarrow@users.noreply.github.com>
2023-01-19 14:04:56 +0200

    Update WorkflowRequest.php

--- src/Requests/Workflow/WorkflowRequest.php
+++ src/Requests/Workflow/WorkflowRequest.php
@@ -7,6 +7,16 @@ use Atlassian\JiraRest\Requests\AbstractRequest;
 class WorkflowRequest extends AbstractRequest
 {
     /**
+     * @param array $options ['cloudId' => string, 'accessToken' => sting]
+     */
+    public function __construct($options = [])
+    {
+        if (isset($options['cloudId']) && isset($options['accessToken'])) {
+            parent::__construct($options);
+        }
+    }
+    
+    /**
      * Returns a list of all statuses associated with workflows.
      *
      * @see https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-status-get
